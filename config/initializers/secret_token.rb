f2820ccaee682e6be0a11af7f79b5eac9452b224
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-07 22:30:33 +0100

    Start new Rails app with PostgreSQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuizApp::Application.config.secret_key_base = '517332e43f4284b0985b04de73757a13cbe9ab53de5f69bd6e7fa1398fe1a697f776f984a99144b246c1bf05e7e9f371086a7c8764e80b222e0ce54f753533b3'
