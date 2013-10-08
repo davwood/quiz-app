362834460a4c2511ea70db6fec92025392e6e996
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-08 12:49:54 +0100

    Deleted manual migration and generated Quiz model

--- db/migrate/20131008114356_create_quizzes.rb
+++ db/migrate/20131008114356_create_quizzes.rb
@@ -2,6 +2,8 @@ class CreateQuizzes < ActiveRecord::Migration
   def change
     create_table :quizzes do |t|
       t.string :title
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008112736) do
+ActiveRecord::Schema.define(version: 20131008114356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "quizzes", force: true do |t|
-    t.string "title"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
