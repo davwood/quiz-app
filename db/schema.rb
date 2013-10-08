abea0621220bdc10709079f235616b5789cd9215
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-08 16:39:17 +0100

    Add bcrypt gem for has_secure_password and generate User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008114356) do
+ActiveRecord::Schema.define(version: 20131008153609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "quizzes", force: true do |t|
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
