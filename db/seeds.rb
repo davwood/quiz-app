c3340510b2f69998637d3259a7c5691b12d3cd20
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-08 15:00:17 +0100

    Add simple seed quiz data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+quizzes = Quiz.create([{ title: 'Pub Quiz' },
+                       { title: 'Health Quiz' }])
