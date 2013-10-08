947bd1aac37a9bb6415c2703bf175dfc336c1d8f
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-08 12:20:43 +0100

    Generate quizzes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 QuizApp::Application.routes.draw do
+
+  resources :quizzes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
