5d5b87bc642d8af9e7f0cb590c083aba53a019b4
Dave Wood <davwood_1@yahoo.com.au>
2013-10-09 16:25:58 +0100

    Sign out link added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,5 +14,8 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
+		flash[:notice] = "Goodbye!"
+		session[:user_id] = nil
+		redirect_to root_path
 	end
 end
--- spec/features/sessions_feature_spec.rb
+++ spec/features/sessions_feature_spec.rb
@@ -8,6 +8,12 @@ def create_user(first_name, last_name, email, password)
           					   password_confirmation: password })
 end
 
+def sign_in(email,password)  
+  fill_in 'Email', with: email
+  fill_in 'Password', with: password
+  click_button 'Sign in'
+end
+
 describe 'user sessions' do
 
   describe 'sign in page' do
@@ -20,12 +26,19 @@ describe 'user sessions' do
       visit sign_in_path
 
       expect(page).to have_content('Sign in')
-      fill_in 'Email', with: 'bob@job.com'
-      fill_in 'Password', with: 'password'
-      click_button 'Sign in'
+      sign_in('bob@job.com','password')
 
       expect(current_url).to eq root_url
       expect(page).to have_content 'Welcome back Bob!'
+    end
+
+    it 'allows user to sign out' do
+      visit sign_in_path
+      sign_in('bob@job.com','password')
+
+      click_link 'Sign out'
+
+      expect(page).to have_content 'Goodbye!'
     end
 
   end
